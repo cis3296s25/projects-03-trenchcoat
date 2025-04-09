5998d7fc93296af84ceaf2a56a17d8879ff58a01
Zachary Ngo <tuh08662@temple.edu>
2025-04-09 14:22:17 -0400

    Sync appState from socket.io to React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import JoinGame from "./JoinGame";
-import GamePage from "./GamePage";
+import { Link, Route, Routes } from "react-router-dom";
 import { io } from "socket.io-client";
+import CreateGame from "./pages/CreateGame";
+import JoinGame from "./pages/JoinGame";
+import Game from "./pages/Game";
+import Lobby from "./pages/Lobby";
 
 function App() {
   const [appState, setAppState] = useState({
@@ -10,8 +12,8 @@ function App() {
     code: null,
     isValid: false,
     socket: null,
-    userList: [],
     joined: false,
+    roomData: null,
   });
 
   useEffect(() => {
@@ -33,20 +35,78 @@ function App() {
     };
   }, []);
 
+  React.useEffect(() => {
+    if (!appState.socket) return;
+
+    appState.socket.on("roomDataUpdated", (roomCode, roomData) => {
+      console.log("Room data updated:", roomCode, roomData);
+      setAppState((prevData) => ({
+        ...prevData,
+        roomData,
+      }));
+    });
+
+    return () => {
+      if (appState.socket && appState?.roomData?.code) {
+        // Leave the room when the component unmounts
+        appState?.socket?.emit("leaveRoom", {
+          inputCode: appState?.roomData?.code,
+        });
+      }
+    };
+  }, [appState.socket, appState.roomData?.code]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>My Game</h1>
+      <header
+        className="App-header"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <Link
+          onClick={() => {
+            setAppState((prevData) => ({
+              ...prevData,
+              roomData: null,
+            }));
+          }}
+          to={"/"}
+        >
+          <h1
+            style={{
+              marginTop: "2rem",
+              fontFamily: "cursive",
+              fontSize: "3rem",
+              color: "#61dafb",
+              textShadow: "2px 2px #000",
+            }}
+          >
+            Skreeble.io
+          </h1>
+        </Link>
       </header>
       <main>
         <Routes>
           <Route
             path="/"
+            element={
+              <CreateGame appState={appState} setAppState={setAppState} />
+            }
+          />
+          <Route
+            path="/join"
             element={<JoinGame appState={appState} setAppState={setAppState} />}
           />
           <Route
-            path="/game"
-            element={<GamePage appState={appState} setAppState={setAppState} />}
+            path="/join/:roomCode"
+            element={<JoinGame appState={appState} setAppState={setAppState} />}
+          />
+          <Route
+            path="/game/:roomCode"
+            element={<Game appState={appState} setAppState={setAppState} />}
+          />
+          <Route
+            path="/lobby/:roomCode"
+            element={<Lobby appState={appState} setAppState={setAppState} />}
           />
         </Routes>
       </main>
