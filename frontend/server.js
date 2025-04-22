15dd7fc724fd4cec5ed1e29001fa9b758b068cdd
peacheyy <tuo18991@temple.edu>
2025-04-22 14:36:34 -0400

    separated .env to work on localhost

--- frontend/server.js
+++ frontend/server.js
@@ -5,13 +5,6 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Simple route for API testing
-app.get('/api/ping', (req, res) => {
-    res.json({ message: 'pong' });
-});
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
